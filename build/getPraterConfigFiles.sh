29074c7e33a03faabe89b8657e6cd8bcc2af8585
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2021-12-20 13:44:57 +0100

    init

---
+++ build/getPraterConfigFiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p tmp
+pushd tmp
+wget -nc https://github.com/rocket-pool/smartnode-install/releases/latest/download/rp-smartnode-install-amd64.tar.xz
+tar -xvf rp-smartnode-install-amd64.tar.xz
+popd
+if [ ! -d "prater" ]; then
+    cp -r tmp/amd64/rp-smartnode-install/network/prater .
+fi
+
