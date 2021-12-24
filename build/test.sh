a5121bbcb9c0ce257086dcf8f147efa357facd04
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2021-12-24 10:08:14 +0100

    wip

--- build/test.sh
+++ build/test.sh
@@ -3,6 +3,8 @@
 
 alias rp="rocketpool -d /usr/local/bin/rocketpoold -c /srv/rocketpool --allow-root"
 
+alias rpd="/usr/local/bin/rocketpoold --config /srv/rocketpool/config.yml --settings /srv/rocketpool/settings.yml api"
+
 mkdir -p /rocketpool/data
 
 
