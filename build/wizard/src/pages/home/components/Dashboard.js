dbc9dd845bd3577f7d86b1590410f599c47181de
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2021-12-26 23:28:14 +0100

    wip

--- build/wizard/src/pages/home/components/Dashboard.js
+++ build/wizard/src/pages/home/components/Dashboard.js
@@ -67,8 +67,7 @@ const Comp = () => {
         setGenerating(false);
         setProgress(`Running key generator.. Hang on to your socks! This might take a minute.`);
         await axios.post(`${config.api.HTTP}/rpd`, { command: "minipool status" }, { timeout: 5 * 60 * 1000 }).then((res) => {
-            setCmdOutput(res.data);
-            alert(res.data);
+            setCmdOutput("<pre>"+res.data+"</pre>");
         })
         
         setGenerating(false);
@@ -101,73 +100,14 @@ const Comp = () => {
                                 <p className="has-text-centered help">Leslie the Launchpad Rhino (<a target="_blank" href="https://launchpad.ethereum.org/">source</a>)</p>
                             </div>
                         </div>
-                        {!cmdOutput && (
+                        {
                             <>
-                                <div className="field">
-                                    <label className="label">Staking key password</label>
-                                    <p className="help">This is the password that will encrypt your keystore - minimum length  =  8 characters</p>
-                                </div>
-                                <div className="field has-addons">
-                                    <div className="control is-expanded">
-                                        <input className="input" type={passwordFieldType} onChange={(e) => { setPassword(e.target.value) }} />
-
-                                    </div>
-                                    <div className="control">
-                                        <a onClick={toggleViewPassword} className="button is-link is-light"><FontAwesomeIcon
-
-                                            className="icon is-small is-right avadoiconpadding"
-                                            icon={passwordFieldIcon}
-                                        />
-                                        </a></div>
-
-
-                                </div>
-
-                                <div className="field">
-                                    <label className="label">Verify Password</label>
-                                    <div className="control">
-                                        <input className="input" type={passwordFieldType} onChange={(e) => { setVerifyPassword(e.target.value) }} />
-                                    </div>
-                                    {password !== verifyPassword && (
-                                        <p className="help is-danger">passwords do not match</p>
-                                    )}
-                                    {password && password.length > 0 && password === verifyPassword && (
-                                        <p className="help is-success">passwords match :)</p>
-                                    )}
-                                </div>
-
-
-                                <div className="field">
-                                    <label className="label">Amount of keys to generate</label>
-                                    <p className="help">This is the amount of times you want to stake 32 ETH</p>
-                                    <div className="control">
-                                        <input className="input" value={amount} type="text" onChange={(e) => { setAmount(e.target.value) }} />
-                                    </div>
-                                </div>
-                                <div className="field">
-                                    <label className="label">mnemonic language</label>
-                                    <div className="control">
-                                        <div className="select">
-                                            <select value={mnemonicLanguage} onChange={(e) => { setMnemonicLanguage(e.target.value) }}>
-                                                <option>chinese_simplified</option>
-                                                <option>chinese_traditional</option>
-                                                <option>czech</option>
-                                                <option>english</option>
-                                                <option>italian</option>
-                                                <option>korean</option>
-                                                <option>spanish</option>
-
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-
                                 <div className="field is-grouped">
                                     <div className="control">
-                                        <button disabled={!readyToGenerate || generating} onClick={() => { generate(password, amount) }} className="button is-link is-light">Generate Keys</button>
+                                        <button disabled={!readyToGenerate || generating} onClick={() => { generate(password, amount) }} className="button is-link is-light">Query rocket pool node status</button>
                                     </div>
                                 </div>
-                            </>)}
+                            </>}
                         {generating && (
                             <>
                                 <p>{progress}</p>
@@ -175,34 +115,17 @@ const Comp = () => {
                             </>
                         )}
 
-                        {cmdOutput &&
+                        {
                             (
                                 <>
-                                    <h1 className="title is-3 has-text-white">Done!</h1>
-                                    <p>
-                                        We have created a zipfile for you containing
-                                    <ul>
-                                            <li>Your staking keys</li>
-                                            <li>Your deposit file you can use on the <a target="_blank" href="https://launchpad.ethereum.org/">Ethereum Launchpad</a> to fund the validator account(s)</li>
-                                            <li>A textfile containing the mnemonic phrase of your wallet (<b>back this up immediately!</b>)</li>
-                                            <li>A textfile containing the password to decrypt your wallet (<b>back this up immediately!</b>)</li>
-                                        </ul>
-                                    </p>
-                                    <br />
-                                    <div className="field">
-
-                                        <DownloadKeys session={wampSession}/>
-                                    </div>
-                                    <p><a onClick={() => { setCmdShowOutput(!cmdShowOutput) }} className=" is-small is-link">show a transcript of key generation</a></p>
-                                    <br />
-                                   {cmdShowOutput && (
+                                   {
                                         <>
                                             <br /><br />
                                             <pre className="transcript">
                                                 {cmdOutput}
                                             </pre>
                                         </>
-                                    )}
+                                    }
                                 </>
                             )
                         }
